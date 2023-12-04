22a313b79b5c6165fc4d589759378676de1b49e9
Eduardo <ramoseduardo.re5@gmail.com>
2023-12-04 00:21:36 -0300

    saulo

--- gulpfile.js
+++ gulpfile.js
@@ -19,11 +19,38 @@
 // exports.tareas = series(css, javascript, minificarHTML);
 // // exports.default y en la terminal solo pongo gulp de esa manera lo que este asociado a ese default lo ejecuta
 
-const {series, src, dest } = require("gulp");
+const { series, src, dest, watch } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const minifyCSS = require("gulp-clean-css");
+const { compileString } = require("sass");
 
-function css() {
-  return src("src/scss/app.scss").pipe("sass()").pipe(dest("./build/css"));
+{
+  function css() {
+    return src("src/scss/app.scss")
+      .pipe(
+        sass({
+          outputStyle: "expanded",
+        })
+      )
+      .pipe(dest("./build/css"));
+  }
+
+  function minificarcss() {
+    return src("src/scss/app.scss")
+      .pipe(
+        sass({
+          outputStyle: "compressed",
+        })
+      )
+      .pipe(minifyCSS())
+      .pipe(dest("./build/css"));
+  }
+
+  function watchArchivo() {
+    watch("src/scss/**/*.scss", css); // * = La carpeta actual - ** = Todos los archivos con esa extencion
+  }
+
+  exports.css = css;
+  exports.minificarcss = minificarcss;
+  exports.watchArchivo = watchArchivo;
 }
-
-exports.css = css;
