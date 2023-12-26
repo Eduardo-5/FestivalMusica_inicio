7659d37c648830c4dceca47fc4bea9f533e6dd2c
Eduardo <ramoseduardo.re5@gmail.com>
2023-12-26 10:03:53 -0300

    saulo

--- gulpfile.js
+++ gulpfile.js
@@ -23,34 +23,53 @@ const { series, src, dest, watch } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const minifyCSS = require("gulp-clean-css");
 const { compileString } = require("sass");
+const imagemin = require("gulp-imagemin");
+const notify = require("gulp-notify");
+const webp = require("gulp-webp");
 
-{
-  function css() {
-    return src("src/scss/app.scss")
-      .pipe(
-        sass({
-          outputStyle: "expanded",
-        })
-      )
-      .pipe(dest("./build/css"));
-  }
-
-  function minificarcss() {
-    return src("src/scss/app.scss")
-      .pipe(
-        sass({
-          outputStyle: "compressed",
-        })
-      )
-      .pipe(minifyCSS())
-      .pipe(dest("./build/css"));
-  }
-
-  function watchArchivo() {
-    watch("src/scss/**/*.scss", css); // * = La carpeta actual - ** = Todos los archivos con esa extencion
-  }
-
-  exports.css = css;
-  exports.minificarcss = minificarcss;
-  exports.watchArchivo = watchArchivo;
+function css() {
+  return src("src/scss/app.scss")
+    .pipe(
+      sass({
+        outputStyle: "expanded",
+      })
+    )
+    .pipe(dest("./build/css"));
 }
+
+function minificarcss() {
+  return src("src/scss/app.scss")
+    .pipe(
+      sass({
+        outputStyle: "compressed",
+      })
+    )
+    .pipe(minifyCSS())
+    .pipe(dest("./build/css"));
+}
+
+function imagenes() {
+  return src("src/img/**/*")
+    .pipe(imagemin())
+    .pipe(dest("./build/img"))
+    .pipe(notify({ message: "Imagen Minificada" }));
+}
+
+function versionWebp() {
+  return src("src/img/**/*")
+    .pipe(webp())
+    .pipe(dest("./build/img"))
+    .pipe(notify({ message: "Version webP lista" }));
+}
+
+function watchArchivo() {
+  watch("src/scss/**/*.scss", css); // * = La carpeta actual - ** = Todos los archivos con esa extencion
+}
+
+exports.css = css;
+exports.minificarcss = minificarcss;
+exports.imagenes = imagenes;
+exports.watchArchivo = watchArchivo;
+exports.versionWebp = versionWebp;
+
+exports.default = series(css, imagenes, versionWebp, watchArchivo);
